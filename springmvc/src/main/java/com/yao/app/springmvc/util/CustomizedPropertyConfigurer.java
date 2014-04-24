6956b9612d28cb135f8388931bb560e78886b5b3
李白路过 <yaolei313@gmail.com>
2014-04-24 19:39:11 +0800

    配置文件变更

---
+++ springmvc/src/main/java/com/yao/app/springmvc/util/CustomizedPropertyConfigurer.java
@@ -0,0 +1,39 @@
+package com.yao.app.springmvc.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+/**
+ * spring3.1之后使用PropertySourcesPlaceholderConfigurer
+ * 
+ * @author summer
+ *
+ */
+public class CustomizedPropertyConfigurer extends PropertyPlaceholderConfigurer {
+
+    private static Map<String, Object> ctxPropertiesMap;
+
+    @Override
+    protected void processProperties(ConfigurableListableBeanFactory beanFactory, Properties props)
+            throws BeansException {
+
+        super.processProperties(beanFactory, props);
+        // load properties to ctxPropertiesMap
+        ctxPropertiesMap = new HashMap<String, Object>();
+        for (Object key : props.keySet()) {
+            String keyStr = key.toString();
+            String value = props.getProperty(keyStr);
+            ctxPropertiesMap.put(keyStr, value);
+        }
+    }
+
+    // static method for accessing context properties
+    public static Object getContextProperty(String name) {
+        return ctxPropertiesMap.get(name);
+    }
+}
