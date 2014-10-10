b8ea801cbb628e634d064ff70f2a0fea45920094
yaolei313 <yaolei313@gmail.com>
2014-10-10 18:31:47 +0800

    fixbug

--- springmvc/src/main/java/com/yao/app/springmvc/web/actions/DecoratorController.java
+++ springmvc/src/main/java/com/yao/app/springmvc/web/actions/DecoratorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DecoratorController {
     @RequestMapping("test")
     public String test(){
+        // fixbug-1010 git practice
         return "test";
     }
 }
