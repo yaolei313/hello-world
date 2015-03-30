1efd9e7c4a93a041b74cf05e344a411a08c8dc97
yaolei313 <yaolei313@gmail.com>
2015-03-30 20:54:54 +0800

    add solution

---
+++ utils/src/main/java/com/yao/app/algorithm/Solution1.java
@@ -0,0 +1,17 @@
+package com.yao.app.algorithm;
+
+public class Solution1 {
+    public double findMedianSortedArrays(int a[], int b[]) {
+        return 0;
+    }
+
+    private double findMedianSortedArraysByRecursion(int[] a, int aLeft, int aLen, int[] b, int bLeft, int bLen,int k) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution1 s = new Solution1();
+        System.out.println(s.findMedianSortedArrays(new int[] {}, new int[] {}));
+    }
+
+}
