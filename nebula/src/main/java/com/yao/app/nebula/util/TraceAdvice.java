21f34b8a2cf55711df274e61cdc8125b6e2e5b91
yaolei313 <yaolei313@gmail.com>
2015-08-17 14:51:16 +0800

    fix

--- nebula/src/main/java/com/yao/app/nebula/util/TraceAdvice.java
+++ nebula/src/main/java/com/yao/app/nebula/util/TraceAdvice.java
@@ -6,16 +6,25 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
 
 @Aspect
-public class TraceAdvice {
+public class TraceAdvice implements Ordered{
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
     @Pointcut("within(com.yao.app.nebula.service..*) && @annotation(com.yao.app.nebula.util.TraceAvailable)")
     private void anyMethod() {}
 
-    @Around(value = "anyMethod() && @annotation(annotation)")
+    /**
+     * http://www.blogjava.net/supercrsky/articles/174368.html
+     * 
+     * @param jp
+     * @param annotation
+     * @return
+     * @throws Throwable
+     */
+    @Around(value = "anyMethod() && @annotation(annotation)", argNames="annotation")
     public Object around(ProceedingJoinPoint jp, TraceAvailable annotation) throws Throwable {
         log.info("get condition");
         
@@ -27,13 +36,21 @@ public class TraceAdvice {
         log.info(jp.getSignature().getDeclaringType().getSimpleName());
         log.info(jp.getSignature().getModifiers()+"");
         
+        // getThis返回类似com.sun.proxy.$Proxy24的对象
         log.info(jp.getThis().getClass().getName());
-        log.info(jp.getThis().getClass().getCanonicalName());
-        log.info(jp.getThis().getClass().getSimpleName());
-        log.info(jp.getThis().getClass().getTypeName());
+        
+        log.info(jp.getTarget().getClass().getCanonicalName());
+        log.info(jp.getTarget().getClass().getSimpleName());
+        log.info(jp.getTarget().getClass().getTypeName());
         //log.info(jp.getThis().getClass().get);
         
-        jp.getTarget().getClass();
+        log.info(jp.toString());
+        log.info(jp.toShortString());
+        log.info(jp.toLongString());
+        log.info(jp.getThis().toString());
+        log.info(jp.getTarget().toString());
+        log.info(jp.getArgs().toString());
+        log.info(jp.getKind());
 
         Object[] args = jp.getArgs();
 
@@ -55,6 +72,12 @@ public class TraceAdvice {
         log.info("call traceutil finished");
 
         return object;
+    }
+
+    @Override
+    public int getOrder() {
+        // 值较低的那个有更高的优先级,控制多个切面在同一个连接点中运行时的顺序
+        return 0;
     }
     
  
