ac67cdfa73efc9d972f69e4b7bd5f542b6797f63
yaolei <yaolei313@gmail.com>
2016-09-18 18:46:35 +0800

    增加注解切面代码example

--- nebula/src/main/java/com/yao/app/nebula/util/TraceAdvice.java
+++ nebula/src/main/java/com/yao/app/nebula/util/TraceAdvice.java
@@ -1,40 +1,52 @@
 package com.yao.app.nebula.util;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+import org.apache.ibatis.annotations.Param;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 
 @Aspect
-public class TraceAdvice implements Ordered{
+public class TraceAdvice implements Ordered {
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
     @Pointcut("within(com.yao.app.nebula.service..*) && @annotation(com.yao.app.nebula.util.TraceAvailable)")
     private void anyMethod() {}
-    
-    /*@Pointcut("execution(* com.yao.app.nebula.web.vo.Student.getName(..))")
-    private void test() {}
-    
-    @Around(value = "test()")
-    public Object testAround(ProceedingJoinPoint jp) throws Throwable {
-    	Object[] args = jp.getArgs();
-    	
-    	System.out.println("before");
-    	
-    	Object object = null;
-        try {
-            object = jp.proceed(args);
-        } finally {
+
+    /*
+     * @Pointcut("execution(* com.yao.app.nebula.web.vo.Student.getName(..))") private void test()
+     * {}
+     * 
+     * @Around(value = "test()") public Object testAround(ProceedingJoinPoint jp) throws Throwable {
+     * Object[] args = jp.getArgs();
+     * 
+     * System.out.println("before");
+     * 
+     * Object object = null; try { object = jp.proceed(args); } finally { }
+     * 
+     * System.out.println("after");
+     * 
+     * return object; }
+     */
+
+    @SuppressWarnings("unchecked")
+    private <A extends Annotation> A findAnnotation(Method method, int index, Class<A> annotationType) {
+        final Annotation[] paramAnnos = method.getParameterAnnotations()[index];
+        for (Annotation aParamAnno : paramAnnos) {
+            if (annotationType.isInstance(aParamAnno)) {
+                return (A) aParamAnno;
+            }
         }
-        
-        System.out.println("after");
-        
-        return object;
-    }*/
+        return null;
+    }
 
     /**
      * http://www.blogjava.net/supercrsky/articles/174368.html
@@ -44,26 +56,40 @@ public class TraceAdvice implements Ordered{
      * @return
      * @throws Throwable
      */
-    @Around(value = "anyMethod() && @annotation(annotation)", argNames="annotation")
+    @Around(value = "anyMethod() && @annotation(annotation)", argNames = "annotation")
     public Object around(ProceedingJoinPoint jp, TraceAvailable annotation) throws Throwable {
         log.info("get condition");
-        
+        Object[] args = jp.getArgs();
+
+        MethodSignature signature = (MethodSignature) jp.getSignature();
+        Method method = signature.getMethod();
+        for (int i = 0; i < args.length; i++) {
+            Param ta = this.findAnnotation(method, i, Param.class);
+            if (ta != null) {
+                System.out.println(ta.value());
+                System.out.println(args[i]);
+            }
+
+        }
+
+
+
         log.info(jp.getSignature().getDeclaringTypeName());
         log.info(jp.getSignature().getName());
         log.info(jp.getSignature().getClass().toGenericString());
         log.info(jp.getSignature().getClass().toString());
         log.info(jp.getSignature().getDeclaringType().getName());
         log.info(jp.getSignature().getDeclaringType().getSimpleName());
-        log.info(jp.getSignature().getModifiers()+"");
-        
+        log.info(jp.getSignature().getModifiers() + "");
+
         // getThis返回类似com.sun.proxy.$Proxy24的对象
         log.info(jp.getThis().getClass().getName());
-        
+
         log.info(jp.getTarget().getClass().getCanonicalName());
         log.info(jp.getTarget().getClass().getSimpleName());
         log.info(jp.getTarget().getClass().getTypeName());
-        //log.info(jp.getThis().getClass().get);
-        
+        // log.info(jp.getThis().getClass().get);
+
         log.info(jp.toString());
         log.info(jp.toShortString());
         log.info(jp.toLongString());
@@ -72,7 +98,6 @@ public class TraceAdvice implements Ordered{
         log.info(jp.getArgs().toString());
         log.info(jp.getKind());
 
-        Object[] args = jp.getArgs();
 
 
         if (annotation == null || !annotation.enabled()) {
@@ -99,6 +124,6 @@ public class TraceAdvice implements Ordered{
         // 值较低的那个有更高的优先级,控制多个切面在同一个连接点中运行时的顺序
         return 0;
     }
-    
- 
+
+
 }
