6ae07208e21b6e9be92dff9f68f104f7d2f184b8
yaolei <yaolei313@gmail.com>
2017-03-23 20:02:49 +0800

    add groovy

--- utils/src/main/java/com/yao/app/script/GroovyStudy.java
+++ utils/src/main/java/com/yao/app/script/GroovyStudy.java
@@ -1,10 +1,20 @@
 package com.yao.app.script;
 
+import groovy.util.Eval;
+
 /**
+ * 如何集成groovy脚本
+ * http://www.groovy-lang.org/integrating.html
+ *
  * Created by yaolei02 on 2017/3/23.
  */
 public class GroovyStudy {
     public static void main(String[] args) {
-        
+        // 1.执行Groovy脚本最简单的方式
+        println(Eval.me("33*3"));
+    }
+
+    public static void println(Object object){
+        System.out.println(object);
     }
 }
