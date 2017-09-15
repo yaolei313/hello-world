0b19596c1deaea73d3c52c5c787a2027cd66a3fd
yaolei <yaolei313@gmail.com>
2017-09-15 19:53:44 +0800

    fix

--- utils/src/main/java/com/yao/app/App.java
+++ utils/src/main/java/com/yao/app/App.java
@@ -2,6 +2,11 @@ package com.yao.app;
 
 import com.google.common.collect.Lists;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Hello world!
  *
@@ -21,5 +26,20 @@ public class App
         Float t = 0.62342323f;
         System.out.println(t);
 
+
+        String RFC1123_DATE = "EEE, dd MMM yyyy HH:mm:ss zzz";
+        SimpleDateFormat format = new SimpleDateFormat(RFC1123_DATE, Locale.US);
+        System.out.println(format.format(new Date()));
+
+        SimpleDateFormat format2 = new SimpleDateFormat(RFC1123_DATE, Locale.CHINA);
+        System.out.println(format2.format(new Date()));
+
+        String dateStr = "Wed, 30 Aug 2017 20:01:15 CST";
+        try {
+            Date date = format.parse(dateStr);
+            System.out.println(format2.format(date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
