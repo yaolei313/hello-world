062cb1857eeb8aba5c3183a1d293335062c847c8
yaolei313 <yaolei313@gmail.com>
2020-02-28 10:37:40 +0800

    inc version

--- zookeeper/src/main/java/com/yao/app/App.java
+++ zookeeper/src/main/java/com/yao/app/App.java
@@ -1,13 +1,16 @@
 package com.yao.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        LOG.info("Hello World!");
     }
 }
