29382f52297c558b89f5d86554c1acdf1cf59102
yaolei313 <yaolei313@gmail.com>
2021-07-22 14:47:08 +0800

    add

--- utils/src/main/java/com/yao/app/resilience4j/CircuitBreakerStudy.java
+++ utils/src/main/java/com/yao/app/resilience4j/CircuitBreakerStudy.java
@@ -4,16 +4,17 @@ import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.SlidingWindowType;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.retry.Retry;
 import io.vavr.control.Try;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.concurrent.TimeoutException;
+import java.util.Random;
 import java.util.function.Supplier;
 
 public class CircuitBreakerStudy {
 
     public static void main(String[] args) {
-
+        test2();
     }
 
     public static void test1() {
@@ -39,14 +40,14 @@ public class CircuitBreakerStudy {
 
         CircuitBreakerConfig name2Config = CircuitBreakerConfig.custom()
             .failureRateThreshold(50)
-            .slowCallRateThreshold(50)
+            .slowCallRateThreshold(80)
             .waitDurationInOpenState(Duration.ofMillis(1000))
             .slowCallDurationThreshold(Duration.ofSeconds(2))
             .permittedNumberOfCallsInHalfOpenState(3)
             .minimumNumberOfCalls(10)
             .slidingWindowType(SlidingWindowType.TIME_BASED)
             .slidingWindowSize(5)
-            .recordExceptions(IOException.class, TimeoutException.class)
+            .recordExceptions(IOException.class)
             .build();
         CircuitBreaker breaker2 = registry.circuitBreaker("name2", name2Config);
 
@@ -56,20 +57,33 @@ public class CircuitBreakerStudy {
         CircuitBreakerConfig config = CircuitBreakerConfig.custom()
             .failureRateThreshold(50)
             .slowCallRateThreshold(50)
-            .waitDurationInOpenState(Duration.ofMillis(1000))
-            .slowCallDurationThreshold(Duration.ofSeconds(2))
-            .permittedNumberOfCallsInHalfOpenState(3)
+            .waitDurationInOpenState(Duration.ofMillis(5000))
+            .slowCallDurationThreshold(Duration.ofSeconds(1))
+            .permittedNumberOfCallsInHalfOpenState(10)
             .minimumNumberOfCalls(10)
             .slidingWindowType(SlidingWindowType.TIME_BASED)
-            .slidingWindowSize(5)
-            .recordExceptions(IOException.class, TimeoutException.class)
+            .slidingWindowSize(10)
             .build();
 
         CircuitBreaker breaker = CircuitBreaker.of("test1", config);
+        Retry retry = Retry.ofDefaults("retry1");
 
-        Supplier<String> supplier = () -> "hello world";
+        Supplier<String> supplier = () -> {
+            Random r = new Random();
+            int i = r.nextInt(10); // [0,9]
+            if (i < 5) {
+                return "hello world";
+            } else {
+                throw new RuntimeException("test");
+            }
+        };
         Supplier<String> decorated = CircuitBreaker.decorateSupplier(breaker, supplier);
-        String result = Try.ofSupplier(decorated).get();
 
+        // decorated = Retry.decorateSupplier(retry, decorated);
+
+        for (int i = 0; i < 10; i++) {
+            Try<String> result = Try.ofSupplier(decorated).recover(throwable -> "recover from exception");
+            System.out.println(i + "," + result.get());
+        }
     }
 }
